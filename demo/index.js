018d2ab381ab550344e31ea6c19d165f9f7b0f56
Ingvar Stepanyan <me@rreverser.com>
2023-12-26 12:30:00 +0000

    Use top level await in the demo

--- demo/index.js
+++ demo/index.js
@@ -20,37 +20,35 @@ const { width, height } = canvas;
 const ctx = canvas.getContext('2d');
 const timeOutput = document.getElementById('time');
 
-(async function init() {
-  // Create a separate thread from wasm-worker.js and get a proxy to its handlers.
-  let handlers = await Comlink.wrap(
-    new Worker(new URL('./wasm-worker.js', import.meta.url), {
-      type: 'module'
-    })
-  ).handlers;
+// Create a separate thread from wasm-worker.js and get a proxy to its handlers.
+let handlers = await Comlink.wrap(
+  new Worker(new URL('./wasm-worker.js', import.meta.url), {
+    type: 'module'
+  })
+).handlers;
 
-  function setupBtn(id) {
-    // Handlers are named in the same way as buttons.
-    let handler = handlers[id];
-    // If handler doesn't exist, it's not supported.
-    if (!handler) return;
-    // Assign onclick handler + enable the button.
-    Object.assign(document.getElementById(id), {
-      async onclick() {
-        let { rawImageData, time } = await handler({
-          width,
-          height,
-          maxIterations
-        });
-        timeOutput.value = `${time.toFixed(2)} ms`;
-        const imgData = new ImageData(rawImageData, width, height);
-        ctx.putImageData(imgData, 0, 0);
-      },
-      disabled: false
-    });
-  }
+function setupBtn(id) {
+  // Handlers are named in the same way as buttons.
+  let handler = handlers[id];
+  // If handler doesn't exist, it's not supported.
+  if (!handler) return;
+  // Assign onclick handler + enable the button.
+  Object.assign(document.getElementById(id), {
+    async onclick() {
+      let { rawImageData, time } = await handler({
+        width,
+        height,
+        maxIterations
+      });
+      timeOutput.value = `${time.toFixed(2)} ms`;
+      const imgData = new ImageData(rawImageData, width, height);
+      ctx.putImageData(imgData, 0, 0);
+    },
+    disabled: false
+  });
+}
 
-  setupBtn('singleThread');
-  if (await handlers.supportsThreads) {
-    setupBtn('multiThread');
-  }
-})();
+setupBtn('singleThread');
+if (await handlers.supportsThreads) {
+  setupBtn('multiThread');
+}
