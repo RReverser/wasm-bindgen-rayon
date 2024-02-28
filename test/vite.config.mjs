8e768fbc4c61286fa4c07fdcf3fda343c0420a4b
Alex <git@pentive.com>
2024-02-28 15:55:19 -0600

    polish

--- test/vite.config.mjs
+++ test/vite.config.mjs
@@ -2,24 +2,21 @@ import { defineConfig } from 'vite';
 import offMainThread from '@surma/rollup-plugin-off-main-thread';
 
 export default defineConfig(({ mode }) => {
-  const plugins =
-    mode === 'production'
-      ? [ offMainThread() ]
-      : []
+	const plugins = mode === 'production' ? [offMainThread()] : [];
 	return {
 		plugins,
-    server:{
-      headers:{
+		server: {
+			headers: {
 				'Cross-Origin-Opener-Policy': 'same-origin',
 				'Cross-Origin-Embedder-Policy': 'require-corp',
 			},
-    },
-    root: './out/bundler-base/',
-    base: './',
-		build: {
-      outDir: '../vite',
-      minify: false,
-      target: 'es2022'
 		},
-	}
-})
+		root: './out/bundler-base/',
+		base: './',
+		build: {
+			outDir: '../vite',
+			minify: false,
+			target: 'es2022',
+		},
+	};
+});
