c5cbdc232b5a7f0821a112d2028ebcfe0e02de7b
Alex <git@pentive.com>
2024-10-04 09:52:54 -0500

    remove server config

--- test/vite.config.mjs
+++ test/vite.config.mjs
@@ -2,12 +2,6 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
 	return {
-		server: {
-			headers: {
-				'Cross-Origin-Opener-Policy': 'same-origin',
-				'Cross-Origin-Embedder-Policy': 'require-corp',
-			},
-		},
 		root: './out/bundler-base/',
 		base: './',
 		build: {
